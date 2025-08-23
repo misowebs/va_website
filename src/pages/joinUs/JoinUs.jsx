82abe8fcffa9be133be2d6247ff140ced65da198
misowebs <yul@misowebs.com>
2025-08-22 21:32:41 -0500

    EventBrite Test Added

--- src/pages/joinUs/JoinUs.jsx
+++ src/pages/joinUs/JoinUs.jsx
@@ -22,7 +22,7 @@ function JoinUs() {
           iframeContainerId: 'eventbrite-widget-container-1623749611799',
 
           // Optional
-          iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
+          iframeContainerHeight: 500,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
           onOrderComplete: exampleCallback  // Method called when an order has successfully completed
         });
       }
